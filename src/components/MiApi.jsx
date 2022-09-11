2e2eceab66cfc6c5a7a587328681954bf7e3fb6a
carola <carolacollao@gmail.com>
2022-09-10 22:57:20 -0300

    solución de detalles

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -3,51 +3,33 @@ import { useState, useEffect } from 'react';
 
 const MiAppi = () => {
 
-  //estado par acarga de la APPI
+  //creamos estado para acarga de la APPI
   const [sismos, setSismos] = useState([]);
 
-  // Estado para capturar el input
+  // creamos estado para capturar el input
   const [inputMagnitud, setInputMagnitud] = useState("");
 
-   // Estado para reverse
-   const [btnReverse, setBtnReverse] = useState("");
+  // creamos estado para capturar actividad del botón reverse
+  const [btnReverse, setBtnReverse] = useState("");
 
-  // LLamamos al función que consume la API al momento de montar elcomponente
+  // LLamamos al función que consume la API al momento de montar el componente
   useEffect(() => {
     consultarInformacion();
   }, []);
-
-  //Efecto cuando el campo email cambia en el formulario
-  useEffect(() => {
-    console.log('tremendo remesón');
-  }, [inputMagnitud]);
-
-    //Efecto boton reverse
-    useEffect(() => {
-      console.log('patas par arriba');
-    }, [setBtnReverse]);
-  
-
 
   //Función que consulta la API
   const consultarInformacion = async () => {
     const url = 'https://api.gael.cloud/general/public/sismos';
     const response = await fetch(url)
     const data = await response.json()
-    console.log('dame un remesón')
+    console.log('ta temblando...')
     console.log(data)
     setSismos([...sismos, ...data]);
-
   }
 
-//Función reverse
-const reversarInformación = async () => {
-  console.log('patas par abajo');
-  sismos.reverse
-
-}
-
-
+  //Función reverse, esto no lo logré
+  const revertirOrden = sismos.reverse();
+  console.log(revertirOrden);
 
 
   return (
@@ -61,11 +43,9 @@ const reversarInformación = async () => {
           name={inputMagnitud}
           value={inputMagnitud}
           onChange={(e) => setInputMagnitud(e.target.value)}
-          
         />
-           <button className='btn btn-primary' onClick={() => reversarInformación()}>reverse</button>
+        <button className='btn btn-primary' onClick={() => revertirOrden()}>Revertir Orden</button>
       </form>
-    
 
       <div className='row' >
         {sismos
@@ -85,18 +65,9 @@ const reversarInformación = async () => {
 
               <div class="card-footer"><small class="text-muted"><p>{s.Fecha}</p></small></div>
             </div>)}
-
       </div>
-
-
-
     </div>
   )
-
-
-
-
-
 }
 
 export default MiAppi
